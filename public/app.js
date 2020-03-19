c3098df31d34c7154a44293ff3e65fb2296b293b
MathiasWP <mathiaswpicker@gmail.com>
2020-03-19 16:25:18 +0100

    fix

--- public/app.js
+++ public/app.js
@@ -7,14 +7,14 @@ import activateReadingMode from './js/lib/activateReadingMode.js';
 import deactivateReadingMode from './js/lib/deactivateReadingMode.js';
 import updateBlogMode from './js/dist/updateBlogMode.js';
 
+const blogScrolling = document.querySelector('.blog-scrolling');
+
+const loadingText = document.createElement('div');
+loadingText.classList.add('loading-text');
+loadingText.textContent = 'Laster...';
+blogScrolling.appendChild(loadingText);
+
 (async () => {
-  const blogScrolling = document.querySelector('.blog-scrolling');
-
-  const loadingText = document.createElement('div');
-  loadingText.classList.add('loading-text');
-  loadingText.textContent = 'Laster...';
-  blogScrolling.appendChild(loadingText);
-
   // Get blogposts from server
   const blogPosts = await getBlogPosts();
 
